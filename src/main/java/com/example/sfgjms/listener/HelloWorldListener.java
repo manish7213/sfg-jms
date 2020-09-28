275b90d0e2b37f0d31c244e240a93ecd055552bd
manish7213 <manish.1si11cs057@gmail.com>
2020-09-28 18:09:26 +0530

    ActiveMq embedded sever impl

---
+++ src/main/java/com/example/sfgjms/listener/HelloWorldListener.java
@@ -0,0 +1,47 @@
+package com.example.sfgjms.listener;
+
+import com.example.sfgjms.config.JmsConfig;
+import com.example.sfgjms.model.HelloWorldMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import java.util.UUID;
+@RequiredArgsConstructor
+@Component
+public class HelloWorldListener {
+
+    private final JmsTemplate jmsTemplate;
+
+    @JmsListener(destination = JmsConfig.MY_QUEUE)
+    public void listen(@Payload HelloWorldMessage helloWorldMessage,
+                       @Headers MessageHeaders headers, Message message) {
+        System.out.println(" I received a  message!!");
+
+        System.out.println(helloWorldMessage);
+
+
+    }
+
+    @JmsListener(destination = JmsConfig.MY_SEND_RCV_QUEUE)
+    public void listenForHello(@Payload HelloWorldMessage helloWorldMessage,
+                       @Headers MessageHeaders headers, Message message) throws JMSException {
+
+        HelloWorldMessage payloadMsg = HelloWorldMessage.builder()
+                .id(UUID.randomUUID())
+                .message("World!")
+                .build();
+
+        jmsTemplate.convertAndSend(message.getJMSReplyTo(), payloadMsg);
+
+
+
+    }
+}
